24666ec5b9edc001ce283a66b03eed3b91de3bcd
HebaAly71 <47098642+HebaAly71@users.noreply.github.com>
2020-02-09 16:21:13 -0800

    final

---
+++ static/js/app challenge.js
@@ -0,0 +1,85 @@
+// import the data from data.js
+const tableData = data;
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+function buildTable(data) {
+  // First, clear out any existing data
+  tbody.html("");
+  // Next, loop through each object in the data
+  // and append a row and cells for each value in the row
+  data.forEach((dataRow) => {
+    // Append a row to the table body
+    let row = tbody.append("tr");
+    // Loop through each field in the dataRow and add
+    // each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
+    }
+    );
+  });
+}
+
+
+// Keep track of all filters
+var filters = [];
+var keyword_pairs = [{ "id": "#datetime", "key": "datetime" },
+{ "id": "#city", "key": "city" },
+{ "id": "#state", "key": "state" },
+{ "id": "#country", "key": "country" },
+{ "id": "#shape", "key": "shape" },]
+function handleClick() {
+  let date = d3.select("#datetime").property("value");
+  let city = d3.select("#city").property("value");
+  let state = d3.select("#state").property("value");
+  let country = d3.select("#country").property("value");
+  let shape = d3.select("#shape").property("value");
+  //let filteredData = tableData;
+  filters = [];
+  if (date) {
+    //console.log(date);
+    filters.push({"id":"datetime", "value":date});
+    //filteredData = filteredData.filter(row => row.datetime === date);
+
+  } else {
+    filters = filters;
+  }
+  if (city) {
+    
+    filters.push({"id":"city", "value":city});
+
+  } 
+  if (state) {
+    filters.push({"id":"state", "value":state});
+
+  } 
+  if (country) {
+    filters.push({"id":"country", "value":country});
+
+  } 
+
+  if (shape) {
+    filters.push({"id":"shape", "value":shape});
+
+  } 
+
+  
+  filterTable();
+}
+
+function filterTable() {
+  let filteredData = tableData;
+  console.log(filteredData);
+  for (const el of filters) {
+    console.log(el);
+    filteredData = filteredData.filter(row => row[el.id] === el.value);
+  }
+
+  buildTable(filteredData);
+}
+
+//Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+// Build the table when the page loads
+buildTable(tableData);
